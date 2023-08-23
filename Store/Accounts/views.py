d050761d9a75362a92eed616611ef0d082c1da24
msm1984 <sadeghmomo2020gmail.com>
2023-08-23 14:08:25 +0330

    login view is create

--- Store/Accounts/views.py
+++ Store/Accounts/views.py
@@ -1,8 +1,8 @@
 from wsgiref import validate
 from django.shortcuts import render,redirect
 from .models import users
-from .forms import registerform
-
+from .forms import registerform, loginform
+from django.contrib.auth import authenticate, login, logout
 
 def register(request):
 
@@ -25,14 +25,19 @@ def login(request):
     
     if request.method =="POST":
         
-        form=registerform(request.POST)   
+        form=loginform(request.POST)   
         if form.is_valid():
-            form.save()
+            credentials = form.cleaned_data
+            user = authenticate(username=credentials['user_name'],
+                            password=credentials['password'])
+            if user:
+                login(request, user)
+                return redirect('register')
             
             return redirect('login')
 
     else:
-        form=registerform()
+        form=loginform()
 
     return render(request,"register.html",{'form':form})
 
