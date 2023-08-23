b3320f7604546b5201583bef03674a25000d38a3
AmirReza <fgg32437@gmail.com>
2023-08-23 13:44:46 +0330

    debug files(directory)

--- Store/Accounts/migrations/0002_alter_users_access.py
+++ Store/Accounts/migrations/0002_alter_users_access.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('Register', '0001_initial'),
+        ('Accounts', '0001_initial'),
     ]
 
     operations = [
