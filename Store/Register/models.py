abe5c0b884f63e0a1a483891370fa7270a7a992d
msm1984 <sadeghmomo2020gmail.com>
2023-08-23 11:15:21 +0330

    access field is finished

--- Store/Register/models.py
+++ Store/Register/models.py
@@ -2,9 +2,15 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class users(models.Model):
+    ACCESS_CHOICES = [
+    (0, 'seller'),
+    (1, 'customer'), 
+    ]
+
     user_name=models.CharField(max_length=20)
     password=models.CharField(max_length=20)
     email=models.EmailField()
     first_name=models.CharField(max_length=20)
     last_name=models.CharField(max_length=20)
-    #access=models.BooleanField(max_length=20)
+    access = models.IntegerField(
+        max_length=1, choices=ACCESS_CHOICES, default=1)
