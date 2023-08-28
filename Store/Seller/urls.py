248fb8f4bb319fba3b2ea69e6def51b000cab7d0
AmirReza <fgg32437@gmail.com>
2023-08-28 18:00:13 +0330

    add path and add necesarry html

--- Store/Seller/urls.py
+++ Store/Seller/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.seller_profile,name='sellerProfile'),
+    path('profile/detail/<int:type>/<int:id>', views.product_detail, name='productDetail')
 ]
