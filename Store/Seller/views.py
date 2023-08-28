799f31898b24759f0a735ef9ac49b61459236f1c
msm1984 <sadeghmomo2020@gmail.com>
2023-08-28 16:12:27 +0330

    urls in Seller app is configured

--- Store/Seller/views.py
+++ Store/Seller/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def seller_profile(request):
+
+    return render(request,"sellerProfile.html")
--- Store/Store/urls.py
+++ Store/Store/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("Accounts.urls")),
+    path('seller/', include("Seller.urls")),
     path('', include("Home.urls")),
     
 ]
