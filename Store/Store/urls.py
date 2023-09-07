a1f83419bb55857d8867e7a35a8fdc756c2347e6
msm1984 <sadeghmomo2020@gmail.com>
2023-09-07 23:01:18 +0330

    pictures are showed in website

--- Store/Store/urls.py
+++ Store/Store/urls.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +11,4 @@ urlpatterns = [
     path('seller/', include("Seller.urls")),
     path('', include("customer.urls")),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
