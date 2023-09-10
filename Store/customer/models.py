c2798bf26730a4812250b4614470ce0b187f0443
AmirReza <fgg32437@gmail.com>
2023-09-10 17:40:21 +0330

    debug Cart Database

--- Store/customer/models.py
+++ Store/customer/models.py
@@ -4,13 +4,13 @@ from Accounts.models import Customer,Seller
 
 
 class Cart(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name="+")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='+')
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
     quantity = models.PositiveIntegerField()
 
 class Order(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name="+")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='+')
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
 
