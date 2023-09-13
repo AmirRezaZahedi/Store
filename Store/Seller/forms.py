a89504a90a7595b87774817d19c2c0e0b8c84350
a6907823415 <a.samadani110@gmail.com>
2023-09-13 09:58:00 -0700

    create form for category

--- Store/Seller/forms.py
+++ Store/Seller/forms.py
@@ -16,7 +16,30 @@ class product_detailform(forms.Form):
     quantity = forms.IntegerField(min_value=0)
     product_quantity = forms.ChoiceField(choices=CHOICES)
 
+
+def dynamic_product_form(intFields, charField, imageField):
+    dynamic_fields = {}
+
+    dynamic_fields.update({field: forms.IntegerField() for field in intFields})
+    dynamic_fields.update({field: forms.CharField() for field in charField})
+    dynamic_fields.update({field: forms.ImageField() for field in imageField})
+
+    dynamic_form_class = type('DynamicProductForm', (product_detailform), dynamic_fields)
+
+    return dynamic_form_class
+
+ 
+
+def dynamic_category_form(category):
+    dynamic_fields = {}
+
+    CHOICES=((i,category[i])  for i in range(len(category)))
+    
+    dynamic_fields.update({category: forms.IntegerField(choices=CHOICES) })
     
 
+    dynamic_form_class = type('DynamicCategoryForm', dynamic_fields)
+
+    return dynamic_form_class
 
      
--- Store/Seller/views.py
+++ Store/Seller/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from .models import Product,staticFeature,dynamicFeature
+from .models import Product,staticFeature,intDynamicFeature,charDynamicFeature,ImageDynamicFeature
 from Accounts.models import User, Seller
 from customer.models import Order 
-from .forms import product_detailform
+from .forms import *
 
 
 def update_product(cd,product):
